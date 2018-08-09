ea16bbeb64c892000bc8ed3609a8d4d0d95d0a6a
mcre <mc@mchs-u.net>
2018-08-09 16:59:48 +0900

    makedic.shをクラウドで直接実行できるように変更

--- dic/makedic.sh
+++ dic/makedic.sh
@@ -1,4 +1,6 @@
-cd ~/GoogleDrive/Documents/programming/java/aiwolf/aiwolf-pre2018-nlp/dic
-/usr/local/Cellar/juman/7.01/libexec/juman/makeint usr.dic
-/usr/local/Cellar/juman/7.01/libexec/juman/dicsort usr.int > jumandic.dat
-/usr/local/Cellar/juman/7.01/libexec/juman/makepat usr.int
+#!/bin/sh
+
+# cd ~/aiwolf-4th-nlp/dic
+~/usr/libexec/juman/makeint usr.dic
+~/usr/libexec/juman/dicsort usr.int > jumandic.dat
+~/usr/libexec/juman/makepat usr.int
